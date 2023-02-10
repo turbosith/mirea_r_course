8fb027aadcaaccee38175a640095fc1231b7a9dd
Данила <93432684+turbosith@users.noreply.github.com>
2023-02-10 15:44:27 +0300

    Initial commit

---
+++ project1.R
@@ -0,0 +1,45 @@
+numeric_var1 <-4
+print("jnfjkfjd")
+is.numeric(numeric_var1)
+is.double(numeric_var1)
+
+complex_var<-5+8.121i
+is.complex(complex_var)
+complex_var[1]
+is.vector(complex_var)
+var_1_10 <-1:10
+var_1_10<-var_1_10*2
+var_1_10<-var_1_10+1:3
+
+
+vec1 <-numeric(12)
+vec1
+vec1 <- character(11)
+vec3 <- 28:2
+vec4<- 0:-12
+vec5<- c(9,3,1,2)
+vec6 <- c("sfsf",1,TRUE)
+vec7<-c(vec5,vec6)
+as.numeric(c("cow","dog"))
+rep(5,10)
+vec9<-rep(5,7)
+#%% скалярное произведение 
+#t - транспонирование 
+
+paste(1:21, c("var","nonvar"))
+
+test_vec <- runif(20,-5,6)
+     
+# индекс с минусом удаляет элемент 
+ 
+
+
+
+
+
+
+
+
+
+
+ 
